495653570aa082f978a08549146410436142f046
mounika <monikadharavath7@gmail.com>
2022-02-13 20:54:07 +0530

    phase 1

---
+++ Bubble Sort/src/bubbleSort.java
@@ -0,0 +1,33 @@
+
+public class bubbleSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	     int[] arr= {251,201,115,15,110};
+	     bubbleSort(arr);
+	     for(int i=0;i<arr.length;i++){
+
+	        System.out.println(arr[i]);
+	        }
+	    }
+
+	    public static void bubbleSort(int[] arr){
+	        int len = arr.length;
+	        int temp = 0;
+	        for(int i=0;i<len;i++){
+	            for (int j=1;j<(len);j++){
+	                if(arr[j-1]>arr[j]){
+	                temp = arr[j-1];
+	                arr[j-1]= arr[j];
+	                arr[j]= temp;
+
+	                }
+
+
+	            }
+
+	        }
+
+	}
+
+}
